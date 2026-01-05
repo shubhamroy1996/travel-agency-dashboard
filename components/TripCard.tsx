46fc43065760ee6a74d6186ffe5a6edb458da822
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2026-01-05 23:03:01 +0530

    add stats card for dashboard

---
+++ components/TripCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TripCard = () => {
+  return (
+    <div>
+      <h1>Trip Card</h1>
+    </div>
+  )
+}
+
+export default TripCard
--- components/index.tsx
+++ components/index.tsx
@@ -1,3 +1,5 @@
 export {default as NavItems} from './NavItems';
 export {default as MobileSidebar} from './MobileSidebar'
 export {default as Headers} from './Headers'
+export {default as StatsCard} from './StatsCard'
+export {default as TripCard} from './TripCard'
