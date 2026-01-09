ebb1c0f055573f122eb7e1879492663610596baf
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2026-01-09 14:29:58 +0530

    add auth functionality

--- app/appwrite/client.ts
+++ app/appwrite/client.ts
@@ -1,3 +1,5 @@
+import { Account, Client, Databases, Storage } from 'appwrite'
+
 export const appwriteConfig = {
   endpointUrl: import.meta.env.VITE_APPWRITE_API_ENDPOINT,
   projectId: import.meta.env.VITE_APPRWRITE_PROJECT_ID,
@@ -5,5 +7,14 @@ export const appwriteConfig = {
   databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
   usersCollectionId: import.meta.env.VITE_APPRWRITE_USERS,
   tripsCollectionId: import.meta.env.VITE_APPWRITE_TRIPS,
-
 }
+
+const client = new Client()
+  .setEndpoint(appwriteConfig.endpointUrl)
+  .setProject(appwriteConfig.projectId)
+
+const account = new Account(client)
+const database = new Databases(client)
+const storage = new Storage(client)
+
+export { client, account, database, storage }
