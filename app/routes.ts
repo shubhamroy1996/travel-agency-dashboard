a63287fac0c15c909bb704a79f1cbbc35818f531
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2026-01-14 22:14:49 +0530

    add routes for trip creation

--- app/routes.ts
+++ app/routes.ts
@@ -5,6 +5,7 @@ import { type RouteConfig, route, layout } from "@react-router/dev/routes";
 
 export default [
   route('sign-in', 'routes/root/sign-in.tsx'),
+  route('api/create-trip', 'routes/api/create-trip.ts'),
 
   layout('routes/admin/admin-layout.tsx', [
     route('Dashboard', 'routes/admin/dashboard.tsx'),
