547158a12ac528bf68719b71dd9424ebfb8513ca
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2026-01-16 14:37:01 +0530

    add popular trip detail

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -45,7 +45,7 @@ const TripCard = ({
             {tags?.map((tag, index) => (
               <ChipDirective
                 key={index}
-                text={getFirstWord(tag)}
+                text={tag}
                 cssClass={cn(
                   index === 1
                     ? "!bg-pink-50 !text-pink-500"
